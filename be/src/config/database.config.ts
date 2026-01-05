25e42e9785f3c58db89e67933a8b070a184685c8
Viet Tran Hoang <vietth2004@gmail.com>
2026-01-05 13:33:04 +0700

    add tsconfig.json

--- be/src/config/database.config.ts
+++ be/src/config/database.config.ts
@@ -8,10 +8,10 @@ export default registerAs(
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '3306', 10),
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'trucdang02',
+    password: process.env.DB_PASSWORD || 'aod@123',
     database: process.env.DB_DATABASE || 'financial1',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    synchronize: false, // Database đã được tạo sẵn, không tự động sync
+    synchronize: true, // Database đã được tạo sẵn, không tự động sync
     logging: process.env.NODE_ENV === 'development',
   }),
 );
